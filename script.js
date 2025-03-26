a43a323de596b598a29e6fc3c86bb776a10d8a4f
KingTiger166 <148370499+kingtiger166@users.noreply.github.com>
2025-03-26 10:51:32 -0300

    wqeequk 7 uymuyuyuyththtrrrrrr

--- script.js
+++ script.js
@@ -42,3 +42,4 @@ document.addEventListener('DOMContentLoaded', function(){
  ScrollReveal().reveal('#tropicalia', { delay: 500 });
  ScrollReveal().reveal('#galeria', { delay: 500 });
  ScrollReveal().reveal('#contato', { delay: 500 });
+ ScrollReveal({ reset: true });
